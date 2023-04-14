5e933d710816c7a94ce28bfaad38958ee2ebd57d
CarlosPast <cpastorgamero@safareyes.es>
2023-04-14 14:15:29 +0200

    illo 2

--- djiangodeprueba/models.py
+++ djiangodeprueba/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 
@@ -29,3 +31,34 @@ class prueba_del_algodon(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class UsuarioManager(BaseUserManager):
+    def create_user(self,email, password, **extra_fields):
+        if not email:
+            raise ValueError("El usuario debe tener un email válido")
+        user = self.model(email=self.normalize_email(email),**extra_fields )
+        user.set_password(password)
+        user.save(using = self.db)
+        return user
+
+    def create_superuser(self,email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault("is_susperuser", True)
+        return self.create_user(email,password,**extra_fields)
+
+
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(unique=True, max_length=255, blank=False)
+    password = models.CharField(max_length=255, blank=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD= 'username'
+    REQUIRED_FIELDS = ['username,password']
+
+    objects = UsuarioManager()
+
+    def __str__(self):
+        return self.username, self.email
--- djiangodeprueba/views.py
+++ djiangodeprueba/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .añadir_datos import *
 # Create your views here.
@@ -9,4 +9,14 @@ def cargar_pagina_inicio(request):
 def registroDatos(request):
     lista_datos = prueba.objects.all()
     lista_datos2 = prueba_del_algodon.objects.all()
-    return render(request,'ListaPrueba.html',{'prueba': lista_datos,"prueba_algodon":lista_datos2})
+
+    if request.method == "GET":
+
+        return render(request,'ListaPrueba.html',{'prueba': lista_datos,"prueba_algodon":lista_datos2})
+    else :
+        nuevo_user = Usuario()
+        nuevo_user.username = request.POST.get("name")
+        nuevo_user.password = request.POST.get("pass")
+        Usuario.save(nuevo_user)
+        return redirect('/djiangodeprueba/prueba')
+
